1e558c4af2ec6c655b9eae6a9bb0234e4324f916
Matt Peperell <matt@peperell.com>
2019-12-18 21:10:23 +0000

    Experimenting with the Collatz conjecture

---
+++ collatz.pl
@@ -0,0 +1,20 @@
+odd(Current, Next) :-
+	1 =:= mod(Current,2), Next is Current * 3 + 1.
+
+even(Current, Next) :-
+	0 =:= mod(Current,2), Next is Current / 2.
+
+step(Current, Next, ThisStep) :-
+	Current \= 1, odd(Current, Next), ThisStep = 'O'.
+
+step(Current, Next, ThisStep) :-
+	even(Current, Next), ThisStep = 'E'.
+
+steps(Start, End, [H|T]) :-
+	step(Start, Interim, H), steps(Interim, End, T).
+
+steps(Start, End, [H|[]]) :-
+	step(Start, End, H), End is 1.
+
+solve(Start, Path) :-
+	steps(Start, 1, Path).
